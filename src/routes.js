0b299d60b74bb4fe178993b6b8d631c50c22b068
Gareth Hunt <ghunt03@gmail.com>
2018-09-18 16:52:30 +1000

    updated calendar route

--- src/routes.js
+++ src/routes.js
@@ -2,4 +2,4 @@ var express = require("express");
 var router = express.Router();
 var calendarController = require("./controller/calendar");
 module.exports = router;
-router.get("/:calendar", calendarController.getCalendar);
+router.get("/", calendarController.getCalendar);
